6af7bedffce238987d6af82fa334fa245b83ccdc
wycj2009 <wycj2009@naver.com>
2021-11-10 14:48:45 +0900

    init

--- app/src/main/java/com/example/android_stateflow_example/TimeRepository.kt
+++ app/src/main/java/com/example/android_stateflow_example/TimeRepository.kt
@@ -8,6 +8,6 @@ class TimeRepository {
     private val timeDataSource: TimeDataSource = TimeDataSource()
 
     val time: Flow<Long> = timeDataSource.latestTime
-            .map { value: Long -> value } // Intermediate operation to filter the list of favorite topics
+        .map { value: Long -> value } // Intermediate operation to filter the list of favorite topics
 //            .onEach { l: Long ->  saveInCache(l) } // Intermediate operation to save the latest news in the cache
 }
